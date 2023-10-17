c8d4bbfc820951481646381692a5dcbe57e495ed
Adrian Azeredo Dos Anjos <adriandosanjos000@gmail.com>
2023-10-17 20:09:44 -0300

    Add files via upload

---
+++ street/sinal/Aplication.java
@@ -0,0 +1,9 @@
+package street.sinal;
+
+import street.sinal.controller.CommandSinal;
+
+public class Aplication {
+	public static void main(String[] args) {
+		CommandSinal.commandSinal();
+	}
+}
---
+++ street/sinal/controller/CommandSinal.java
@@ -0,0 +1,6 @@
+package street.sinal.controller;
+
+public class CommandSinal {
+	public static void commandSinal() {
+	}
+}
