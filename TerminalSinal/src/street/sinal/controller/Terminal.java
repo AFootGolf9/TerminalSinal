58bc791a088e807439cb2241de681e2c633ee988
Adrian Azeredo Dos Anjos <adriandosanjos000@gmail.com>
2023-10-19 21:29:06 -0300

    new things

---
+++ TerminalSinal/src/street/sinal/controller/Terminal.java
@@ -0,0 +1,25 @@
+package street.sinal.controller;
+
+import java.util.Scanner;
+
+public class Terminal {
+
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static String readColor(String label) {
+        String valor = "";
+        var invalid = false;
+        do {
+            try {
+                System.out.print(label);
+                String timeRead = SC.nextLine();
+                Integer.parseInt(timeRead);
+                invalid = true;
+                System.out.println("WARNING> THIS VALLUE IS INVALID!");
+	        } catch (NumberFormatException ex) {
+                invalid = false;
+            }
+        } while (invalid);
+        return valor;
+    }
+}
